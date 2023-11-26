044aa540798511ee7d4d763860bc3bb83d2e692f
Makhortykh-Ruslan <59147778+Makhortykh-Ruslan@users.noreply.github.com>
2023-11-26 15:41:50 +0200

    add fix

--- server.js
+++ server.js
@@ -85,6 +85,6 @@ if (process.env.NODE_ENV === 'production') {
 
 //
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
