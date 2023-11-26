ce503f5290e6c333a1a335b6d62c819d39988817
Makhortykh-Ruslan <59147778+Makhortykh-Ruslan@users.noreply.github.com>
2023-11-26 23:36:04 +0200

    add fix

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongoURI:
-  'mongodb+srv://Ruslan:Ruslan@test-online-shop.fffe6.mongodb.net/?retryWrites=true&w=majority',
-     // 'mongodb+srv://Ruslan:chillerPassword@test-online-shop.fffe6.mongodb.net/test-online-shop?retryWrites=true&w=majority',
+  // 'mongodb+srv://Ruslan:Ruslan@test-online-shop.fffe6.mongodb.net/?retryWrites=true&w=majority',
+     'mongodb+srv://Ruslan:chillerPassword@test-online-shop.fffe6.mongodb.net/test-online-shop?retryWrites=true&w=majority',
   secretOrKey: 'random very very secret string',
 };
