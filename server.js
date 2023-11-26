af45f898c798942a68c9c5d6b700c9ed51c38111
Makhortykh-Ruslan <59147778+Makhortykh-Ruslan@users.noreply.github.com>
2023-11-27 00:14:17 +0200

    add fix

--- server.js
+++ server.js
@@ -85,7 +85,6 @@ if (process.env.NODE_ENV === 'production') {
 
 //
 
-// const port = process.env.PORT || 4000;
-const port = 'https://e-commerce-backend-livid-theta.vercel.app/'
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
