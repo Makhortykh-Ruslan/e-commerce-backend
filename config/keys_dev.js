dd9057ed1462accef4d26953c95e774b1a56e306
Makhortykh-Ruslan <59147778+Makhortykh-Ruslan@users.noreply.github.com>
2023-11-29 23:34:48 +0200

    add fix

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,11 @@
 module.exports = {
-  mongoURI:
-     'mongodb+srv://Ruslan:chillerPassword@test-online-shop.fffe6.mongodb.net/test-online-shop?retryWrites=true&w=majority',
+  mongoURI: '${process.env.MONGODB_URI}',
+     // 'mongodb+srv://${process.env.MONGODB_API_KEY}@test-online-shop.fffe6.mongodb.net/test-online-shop?retryWrites=true&w=majority',
   secretOrKey: 'random very very secret string',
 };
+
+//mongodb+srv://vercel-admin-user:otwa4Mkta7qBHlo6@test-online-shop.fffe6.mongodb.net/test-online-shop?retryWrites=true&w=majority
+
+//4d736bb4-7c97-408a-8fa8-6318a4226892
+
+//hxidnyyl
--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,6 +1,5 @@
 module.exports = {
-  // mongoURI: process.env.MONGODB_URI,
-  mongoURI: 'mongodb+srv://Ruslan:chillerPassword@test-online-shop.fffe6.mongodb.net/test-online-shop?retryWrites=true&w=majority',
+  mongoURI: process.env.MONGODB_URI,
   secretOrKey: process.env.SECRET_OR_KEY,
   nodemailerUser: process.env.NODEMAILER_USER,
   nodemailerPassword: process.env.NODEMAILER_PASSWORD,
