65e0b6ace535609f41b0e3a7e88553f455f611c8
Makhortykh-Ruslan <59147778+Makhortykh-Ruslan@users.noreply.github.com>
2023-11-29 19:11:59 +0200

    add fix

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,6 @@
 module.exports = {
-  mongoURI: process.env.MONGODB_URI,
+  // mongoURI: process.env.MONGODB_URI,
+  mongoURI: 'mongodb+srv://Ruslan:chillerPassword@test-online-shop.fffe6.mongodb.net/test-online-shop?retryWrites=true&w=majority',
   secretOrKey: process.env.SECRET_OR_KEY,
   nodemailerUser: process.env.NODEMAILER_USER,
   nodemailerPassword: process.env.NODEMAILER_PASSWORD,
