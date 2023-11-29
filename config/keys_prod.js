79775f13764714544c1400405113935dda9d7587
Makhortykh-Ruslan <59147778+Makhortykh-Ruslan@users.noreply.github.com>
2023-11-29 19:19:40 +0200

    add remove prot

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,8 +1,7 @@
 module.exports = {
   // mongoURI: process.env.MONGODB_URI,
   mongoURI: 'mongodb+srv://Ruslan:chillerPassword@test-online-shop.fffe6.mongodb.net/test-online-shop?retryWrites=true&w=majority',
-  secretOrKey: 'PORT',
-  // secretOrKey: process.env.SECRET_OR_KEY,
+  secretOrKey: process.env.SECRET_OR_KEY,
   nodemailerUser: process.env.NODEMAILER_USER,
   nodemailerPassword: process.env.NODEMAILER_PASSWORD,
   nodemailerService: process.env.NODEMAILER_SERVICE
