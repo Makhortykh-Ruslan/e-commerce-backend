654c4b705b911386ecfea761708e2cae7ba284c9
Makhortykh-Ruslan <59147778+Makhortykh-Ruslan@users.noreply.github.com>
2023-11-29 19:17:07 +0200

    add PORT

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,7 +1,8 @@
 module.exports = {
   // mongoURI: process.env.MONGODB_URI,
   mongoURI: 'mongodb+srv://Ruslan:chillerPassword@test-online-shop.fffe6.mongodb.net/test-online-shop?retryWrites=true&w=majority',
-  secretOrKey: process.env.SECRET_OR_KEY,
+  secretOrKey: 'PORT',
+  // secretOrKey: process.env.SECRET_OR_KEY,
   nodemailerUser: process.env.NODEMAILER_USER,
   nodemailerPassword: process.env.NODEMAILER_PASSWORD,
   nodemailerService: process.env.NODEMAILER_SERVICE
